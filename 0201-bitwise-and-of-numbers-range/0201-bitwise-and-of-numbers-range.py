140b83215f12153357be46e5ab2c6e79d31a7a32
shivam <ssharma4@wpi.edu>
2024-10-18 16:48:38 -0400

    Time: 3 ms (100%), Space: 16.7 MB (18.59%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        i=0
+        while left!=right:
+            i+=1
+            left>>=1
+            right>>=1
+        
+
+        return left<<i
