80b969b71e9a9e5c4abc4d24ec64d761f1972f54
shivam <ssharma4@wpi.edu>
2024-10-21 16:21:50 -0400

    Time: 0 ms (100%), Space: 8.4 MB (15.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double recur(double x, int n){
+        if(x==0){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+
+        double ans=recur(x*x, n/2);
+
+        return n%2==0 ? ans : x*ans;
+    }
+    
+    double myPow(double x, int n) {
+
+        double ans=recur(x,std::abs(n));
+
+        return n<0 ? 1/ans : ans;
+
+        
+    }
+};
