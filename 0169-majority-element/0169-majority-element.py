7e4f87d2befcecadd0d0bbce10e2fd8b27c28220
shivam <ssharma4@wpi.edu>
2024-10-27 16:58:43 -0400

    Time: 15 ms (19.62%), Space: 18.4 MB (10.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,19 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-
-        n= len(nums)
-        if n<2:
-            return nums[0]
-        d={}
+        n = len(nums)
+        d = {}
         for i in nums:
-            if i in d and d[i]+1<=n//2:
-                d[i]+=1
-            elif i in d and d[i]+1>n//2:
+            d[i] = d.get(i, 0) + 1
+            if d[i] > n // 2:
                 return i
-            else:
-                d[i]=1
-
-        return None
+        
+        # This line should never be reached if a majority element always exists
+        return max(d, key=d.get)
         
             
         
