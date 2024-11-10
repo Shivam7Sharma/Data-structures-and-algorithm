cfa1e9c2b32bc0d9bcd119f9b0823a23981d9549
shivam <ssharma4@wpi.edu>
2024-11-10 13:33:22 -0500

    Time: 0 ms (100%), Space: 11.2 MB (90.25%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        for(int i=0; i<n-1; i++){
+            for(int j=0;j<=i;j++){
+                if(j==0){
+                    triangle[i+1][j]+=triangle[i][j];
+                }
+                if(j+1<=i)
+                triangle[i+1][j+1]+=min(triangle[i][j],triangle[i][j+1]);
+                else{
+                triangle[i+1][j+1]+=triangle[i][j];
+            }
+            }
+        }
+        return *min_element(begin(triangle[n-1]), end(triangle[n-1]));
+    }
+};
