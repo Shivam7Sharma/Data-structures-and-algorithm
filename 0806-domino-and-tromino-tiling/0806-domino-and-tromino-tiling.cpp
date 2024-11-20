9d590f58545a40b3b2e45bdd921a56cc1d291e13
shivam <ssharma4@wpi.edu>
2024-11-20 11:18:39 -0500

    Time: 0 ms (100%), Space: 7.9 MB (32.57%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
+    int numTilings(int n) {
 
- int numTilings(int N) {
-    int md=1e9;
-    md+=7;
-    vector<long long> v(1001,0);
-    v[1]=1;
-    v[2]=2;
-    v[3]=5;
-    if(N<=3)
-        return v[N];
-    for(int i=4;i<=N;++i){
-        v[i]=2*v[i-1]+v[i-3]; 
-        v[i]%=md;
-    }
-    return v[N];
-    
-}
-    // int numTilings(int n) {
+        vector<long long> dp(1001,0);
+        int md= 1e9;
+        md+=7;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
 
-    //     vector<long long> dp(1001,0);
-    //     int md= 1e9;
-    //     md+=7;
-    //     dp[1]=1;
-    //     dp[2]=2;
-    //     dp[3]=5;
+        if(n<=3){
+            return dp[n];
+        }
 
-    //     if(n<=3){
-    //         return dp[n];
-    //     }
-
-    //     for(int i=4; i<=n;i++){
-    //         dp[i]= dp[i-3]+2*dp[n-1];
-    //         dp[i]%=md;
-    //     }
-    //     return dp[n];
+        for(int i=4; i<=n;++i){
+            dp[i]= dp[i-3]+2*dp[i-1];
+            dp[i]%=md;
+        }
+        return dp[n];
         
-    // }
+    }
 };
