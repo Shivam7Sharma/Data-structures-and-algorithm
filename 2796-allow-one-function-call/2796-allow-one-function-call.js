64705a1adeb235e107286091d17394ddb0ad7a9b
shivam <ssharma4@wpi.edu>
2024-11-28 18:05:40 -0500

    Time: 53 ms (53.2%), Space: 48.7 MB (77.68%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    var b= true;
+    
+    return function(...args){
+        if(b){
+            b= false;
+            return fn(...args);
+        }
+        else{
+            return;
+        }
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
