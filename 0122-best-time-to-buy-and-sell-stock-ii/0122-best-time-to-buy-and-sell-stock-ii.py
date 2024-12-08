6bf03b55672eaa418d814667e8a3f70f1a9b0235
shivam <ssharma4@wpi.edu>
2024-12-08 13:07:57 -0500

    Time: 5 ms (12.7%), Space: 18.1 MB (21.96%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        lastsold=0
+        lastbuy= -prices[0]
+
+        for i in range(1,len(prices)):
+            currbuy= max(lastbuy, -prices[i]+lastsold)
+            currsell= max(lastsold,prices[i]+lastbuy)
+
+            lastbuy= currbuy
+            lastsold= currsell
+            
+
+        return lastsold
