e901352876cc52d2c98c3caeb8d1d73c251f9403
shivam <ssharma4@wpi.edu>
2024-12-11 17:40:52 -0500

    Time: 0 ms (100%), Space: 10 MB (43.96%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    void backtrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& state, int i){
+        if(state.size()>= nums.size()){
+            return;
+        }
+
+        for( int j=i ; j<nums.size() ; j++){
+            state.push_back(nums[j]);
+            ans.push_back(state);
+            backtrack(nums, ans, state,j+1);
+            
+            state.pop_back();} 
+        }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> state;
+        unordered_set<int> set;
+        ans.push_back({});
+        backtrack(nums, ans, state, 0);
+
+
+        
+        return ans;
+    }
+};
